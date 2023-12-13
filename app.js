c86ec1bc48906e7fac2d3ab65f4e2758ec9583e5
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2023-12-13 16:27:56 -0300

    Finish

--- app.js
+++ app.js
@@ -1,68 +1,98 @@
-const userDayInput = document.querySelector("#day");
-const userMonthInput = document.querySelector("#month");
-const userYearInput = document.querySelector("#year");
+//OUTPUT ELEMENTS
+const output_year = document.querySelector(".output-year");
+const output_month = document.querySelector(".output-month");
+const output_day = document.querySelector(".output-day");
+const submit_btn = document.querySelector(".submit-btn");
 
-const dayOutput = document.querySelector(".userDays");
-const monthOutput = document.querySelector(".userMonths");
-const yearOutput = document.querySelector(".userYears")
+//INPUT ELEMENTS
+let isValid = false;
+const input_year = document.querySelector("#year");
+const input_day = document.querySelector("#day");
+const input_month = document.querySelector("#month");
 
-const btn = document.querySelector("#icon");
+//ERROR ELEMENTS
+const error_day = document.querySelector(".error-day");
+const error_month = document.querySelector(".error-month");
+const error_year = document.querySelector(".error-year");
 
-let todayString = new Date().toISOString().split("T")[0];
+submit_btn.addEventListener("click", CalculateDate);
 
-let todayArray = todayString.split("-")
-
-
-const date = new Date();
-const [month, day, year] = [
-   date.getMonth() + 1,
-   date.getDate(),
-   date.getFullYear(),
-];
-
-btn.addEventListener("click", ()=>{
-   dayValue = userDayInput.value;
-   monthValue = userMonthInput.value;
-   yearValue = userYearInput.value;
-   let para = document.createElement("p")
-   let unvalid = document.querySelectorAll(".unvalid")
-
-   let yearDif, monthDif, dayDif;
-
-   yearDif = year - yearValue;
-   monthDif = month - monthValue;
-   dayDif = day - dayValue;
-
-   let arrayToday = [userYearInput.value, userMonthInput.value, userDayInput.value]
-
-   console.log(arrayToday)
-   
-
-   if(dayValue > 31){
-   } else if (monthValue > 12){
-   } else if(yearValue > year){
-   } else{
-      if(dayDif < 1){
-         monthOutput.textContent = monthDif -1;
-         yearOutput.textContent = yearDif;
-         if(month === 4 || month === 6 || month === 9 || month === 11 ){
-            dayOutput.textContent =  dayDif + 30;
-         } else if(month === 2){
-            dayOutput.textContent = dayDif + 28;
-         } else{
-            dayOutput.textContent = dayDif + 31;
-         }
-      } else{ 
-         dayOutput.textContent =  dayDif;
-         monthOutput.textContent = monthDif;
-         yearOutput.textContent = yearDif;
-      }
+input_day.addEventListener("input", (e)=>{
+   if(+input_day.value > 31){
+      error_day.textContent = "Must be a valid date";
+      isValid = false;
+      return;
+   } else {
+      isValid = true;
+      error_day.textContent = "";
    }
+   if(+input_day.value === 0){
+      isValid = false;
+      error_day.textContent = "This field is required";
+      isValid = false;
+      return;
+   } else {
+      error_day.textContent = "";
+   }
+});
 
-})
-let ageDif = new Object();
-const [userMonth, userDay, userYear] = [
-   userMonthInput.value,
-   userDayInput.value,
-   userYearInput.value,
-];
+input_month.addEventListener("input", (e)=>{
+   if(+input_month.value > 12){
+      error_month.textContent = "Must be a valid date"
+      isValid = false;
+      return;
+   } else{
+      isValid = true;
+      error_month.textContent = "";
+   }
+   if(+input_month.value === 0){
+      isValid = false;
+      error_month.textContent = "This field is required";
+      isValid = false;
+      return;
+   } else {
+      error_month.textContent = "";
+   }
+});
+
+//CURRENT DATE
+let date = new Date;
+let current_year = date.getFullYear();
+let current_month = (date.getUTCMonth() + 1);
+let current_day = date.getUTCDate();
+
+input_year.addEventListener("input", (e)=>{
+   if(+input_year.value > current_year){
+      error_day.textContent = "Must be a valid date";
+      isValid = false;
+      return;
+   } else{
+      isValid= true;
+      error_year.textContent = "";
+   }
+   if(+input_year.value === 0){
+      isValid = false;
+      return;
+   } else{
+      error_year.textContent = "";
+   }
+});
+
+function CalculateDate(){
+   if(isValid){
+      let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`;
+      console.log(birthday);
+      let birthdayObj = new Date(birthday);
+      let ageDiffMill = Date.now() - birthdayObj;
+      let ageDate = new Date(ageDiffMill);
+      let ageYears = ageDate.getUTCFullYear() - 1970;
+      let ageMonth = ageDate.getUTCMonth();
+      let ageDay = ageDate.getDate();
+
+      output_day.textContent = ageDay;
+      output_month.textContent = ageMonth;
+      output_year.textContent = ageYears;
+   } else {
+      alert("error");
+   }
+}
